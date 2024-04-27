061f82e5312087c1c037d18f0b88ea071af73c85
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-04-27 18:04:21 -0400

    adding buildpack

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongodb+srv://mvs1027:Qs5j27101991!@cluster0.uqds3we.mongodb.net/ 
+// mongodb+srv://mvs1027:<password>@cluster0.uqds3we.mongodb.net/
 
+//const uri = process.env.MONGODB_URI;
 module.exports = mongoose.connection;
